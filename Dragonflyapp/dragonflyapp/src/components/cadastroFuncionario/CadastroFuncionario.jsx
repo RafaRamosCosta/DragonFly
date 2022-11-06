fd9c80e0660e84af83c6fcf6fa512ed229e6fa10
RafaRamosCosta <rm94281@fiap.com.br>
2022-11-05 23:17:49 -0300

    Componentes de cadastro de Produtos e Funcionarios criados. Co-authored-by: Leonardo Ramos Costa <LeonardoRCosta@users.noreply.github.com>

--- Dragonflyapp/dragonflyapp/src/components/cadastroFuncionario/CadastroFuncionario.jsx
+++ Dragonflyapp/dragonflyapp/src/components/cadastroFuncionario/CadastroFuncionario.jsx
@@ -1,30 +1,46 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import { CadDiv, Form, Button } from './CadastroFuncionarioStyle';
 
 export default function CadastroFuncionario() {
+  const [novoFuncionario, setNovoFuncionario] = useState({});
+
+  const handleInputChange = (e) => {
+    setNovoFuncionario({ ...novoFuncionario, [e.target.name]: e.target.value });
+    console.log(novoFuncionario);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios({
+      method: 'POST',
+      url: 'http://localhost:8080/DragonflyAPI/rest/funcionario/',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: novoFuncionario,
+    });
+  };
+
   return (
     <CadDiv>
-      <Form method="post">
+      <Form method="post" onSubmit={handleSubmit}>
         <h1>Cadastre-se, Funcionário!</h1>
 
-        <label htmlFor="nmFuncionarop">Nome Completo</label>
-        <input
-          type="text"
-          name="nmFuncionarop"
-          placeholder="Nome e Sobrenome"
-        />
+        <label htmlFor="nmFuncionaro">Nome Completo</label>
+        <input type="text" name="nmFuncionaro" placeholder="Nome e Sobrenome" onChange={handleInputChange}/>
 
         <label htmlFor="cpf">CPF</label>
-        <input type="number" name="cpf" placeholder=" XXX.XXX.XXX-XX" />
+        <input type="number" name="cpf" placeholder=" XXX.XXX.XXX-XX" onChange={handleInputChange}/>
 
         <label htmlFor="dtNascimento">Data de Nascimento</label>
-        <input type="date" name="dtNascimento" placeholder="DD/MM/YYYY" />
+        <input type="date" name="dtNascimento" placeholder="DD/MM/YYYY" onChange={handleInputChange}/>
 
         <label htmlFor="login">Login</label>
-        <input type="text" name="login" placeholder="Login" />
+        <input type="text" name="login" placeholder="Login" onChange={handleInputChange}/>
 
         <label htmlFor="senha">Senha</label>
-        <input type="password" name="senha" placeholder="Senha" />
+        <input type="password" name="senha" placeholder="Senha" onChange={handleInputChange}/>
 
         <div className="divButtons">
           <Button type="submit">Cadastrar</Button>
--- Dragonflyapp/dragonflyapp/src/components/cadastroProduto/ CadastroProduto.jsx
+++ Dragonflyapp/dragonflyapp/src/components/cadastroProduto/ CadastroProduto.jsx
@@ -1,23 +1,57 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState } from 'react';
 import { DivForm, FormProd } from './CadastroProdutoStyle';
 
 export default function CadastroProduto() {
+  const [novoProduto, setNovoProduto] = useState({
+    nmProduto: '',
+    dsProduto: '',
+    vlUnitario: '',
+  });
+
+  const handleInputChange = (e) => {
+    setNovoProduto({ ...novoProduto, [e.target.name]: e.target.value });
+    console.log(novoProduto);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios({
+      method: 'POST',
+      url: 'http://localhost:8080/DragonflyAPI/rest/produto/',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: novoProduto,
+    });
+  };
   return (
     <DivForm>
       <h1>Cadastre um Produto!</h1>
-      <FormProd>
+      <FormProd onSubmit={handleSubmit}>
         <label htmlFor="nmProduto">Nome do Produto</label>
-        <input type="text" name="nmProduto" placeholder="Ex.: Drone X" />
+        <input
+          type="text"
+          name="nmProduto"
+          placeholder="Ex.: Drone X"
+          onChange={handleInputChange}
+        />
 
         <label htmlFor="dsProduto">Descrição do Produto</label>
         <textarea
           name="dsProduto"
           cols="30"
           rows="10"
-          placeholder="Ex.: Espeficações técnicas"></textarea>
+          placeholder="Ex.: Espeficações técnicas"
+          onChange={handleInputChange}></textarea>
 
         <label htmlFor="vlUnitario">Valor Unitário</label>
-        <input type="number" name="vlUnitario" placeholder="R$" />
+        <input
+          type="number"
+          name="vlUnitario"
+          placeholder="R$"
+          onChange={handleInputChange}
+        />
 
         <button type="submit">Cadastrar</button>
         <button type="reset">Limpar</button>

934d20c4d4646c150349319348632d9684dab25a
RafaRamosCosta <rm94281@fiap.com.br>
2022-11-05 23:20:45 -0300

    Rotas atualizadas. Co-authored-by: Leonardo Ramos Costa <LeonardoRCosta@users.noreply.github.com>

--- Dragonflyapp/dragonflyapp/src/components/routes/Rotas.jsx
+++ Dragonflyapp/dragonflyapp/src/components/routes/Rotas.jsx
@@ -4,9 +4,11 @@ import { Route, Routes } from 'react-router-dom';
 import Cadastro from '../cadastro/Cadastro';
 import CadastroFuncionario from '../cadastroFuncionario/CadastroFuncionario';
 import CadastroProduto from '../cadastroProduto/ CadastroProduto';
-import Produto from '../produto/Produto';  
+import Produto from '../produto/Produto';
 import Login from '../login/Login';
 import Home from '../home/Home';
+import CadastroEndereco from '../cadastroEndereco/CadastroEndereco';
+import CadastroContato from '../cadastroContato/CadastroContato';
 
 export default function Rotas() {
   return (
@@ -14,9 +16,11 @@ export default function Rotas() {
       <Route path="/" element={<Home />} />
       <Route path="/produtos" element={<Produto />} />
       <Route path="/cadastro" element={<Cadastro />} />
+      <Route path="/cadastroEnderecoEmpresa" element={<CadastroEndereco />} />
+      <Route path="/cadastroContatoEmpresa" element={<CadastroContato />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/cadastro_produto" element={<CadastroProduto />} />
-      <Route path="/cadastro_funcionario" element={<CadastroFuncionario />} />
+      <Route path="/cadastroProduto" element={<CadastroProduto />} />
+      <Route path="/cadastroFuncionario" element={<CadastroFuncionario />} />
     </Routes>
   );
 }

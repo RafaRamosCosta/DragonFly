28f35438f73964dd7a93a7dd7cb9bf0bd6880c4c
RafaRamosCosta <rm94281@fiap.com.br>
2022-11-05 23:19:28 -0300

    Componente Produto retornando os produtos do banco, e estilizado com responsividade. Co-authored-by: Leonardo Ramos Costa <LeonardoRCosta@users.noreply.github.com>

--- Dragonflyapp/dragonflyapp/src/components/produto/Produto.jsx
+++ Dragonflyapp/dragonflyapp/src/components/produto/Produto.jsx
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
-import {DivProd} from './ProdutoStyle';
-
+import { DivProd, PageProd } from './ProdutoStyle';
 
 export default function Produto() {
-    return (
-        <DivProd>
-            <h1>Drone XXX</h1>
-            <p>"<img src="imagem_drone" alt="" />"</p>
-            <h2>Descrição do Produto</h2>
-            <p style={{color: "#fffa6b"}}>Made In Heaven</p>
-            <h2>Preço do Produto</h2>
-            <p style={{color: "#ff0400"}}>Highway to Hell</p>
-            <Link to="algum_lugar_pra_relaxar" className='compra'>🛒Comprar</Link>
-        </DivProd>
+  const userData = sessionStorage.getItem("emp");
+
+  const [produtos, setProdutos] = useState([]);
+  useEffect(() => {
+    axios('http://localhost:8080/DragonflyAPI/rest/produto').then((response) =>
+      setProdutos(response.data)
     );
+    if (!userData) {
+      window.location = "/login";
+    }
+  }, []);
+
+  
+  return (
+    <PageProd>
+      <section>
+        <h1>Tutorial de Pilotagem</h1>
+      </section>
+      {produtos.map((produto, i) => (
+        <DivProd key={i}>
+          <h1>{produto.nmProduto}</h1>
+          <p>
+            "<img src="imagem_drone" alt="" />"
+          </p>
+          <h2>Descrição do Produto</h2>
+          <p style={{ color: '#fffa6b' }}>{produto.dsProduto}</p>
+          <h2>Preço do Produto</h2>
+          <p style={{ color: '#fff' }}>R$ {produto.vlUnitario}</p>
+          <Link to="algum_lugar_pra_relaxar" className="compra">
+            🛒Comprar
+          </Link>
+        </DivProd>
+      ))}
+    </PageProd>
+  );
 }
--- Dragonflyapp/dragonflyapp/src/components/produto/ProdutoStyle.js
+++ Dragonflyapp/dragonflyapp/src/components/produto/ProdutoStyle.js
@@ -1,30 +1,49 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
+export const PageProd = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  row-gap: 20px;
+  padding: 20px;
+`;
 export const DivProd = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  background-color: #0f521d;
+  border-radius: 7px;
+  max-width: 500px;
+  row-gap: 2px;
+  .compra {
+    text-align: center;
+    border: none;
+
+    background-color: #0c5c11;
+    cursor: pointer;
+
     padding: 10px;
-    margin: 40px;
-    background-color: #0f521d;
-    border-radius: 7px;
-    .compra{
-        text-align: center;
-        border: none;
-
-        background-color: #0c5c11;
-        cursor: pointer;
-        margin-top: 10px;
-        padding: 10px;
-        border-radius: 5px;
-        color: #fff;
-        width: 40%;
-        transition: all 0.3s ease-in-out;
-        :hover {
-            background-color: #1c8d19;
-            transform: scale(1.1);
-        }
+    border-radius: 5px;
+    color: #fff;
+    width: 40%;
+    transition: all 0.3s ease-in-out;
+    :hover {
+      background-color: #1c8d19;
+      transform: scale(1.1);
     }
-
+  }
+  @media screen and (max-width: 550px) {
+    h1 {
+        font-size: 1.5rem;
+    }
+    h2 {
+      font-size: 1.2rem;
+    }
+    p {
+      font-size: 0.8rem;
+    }
+  }
 `;

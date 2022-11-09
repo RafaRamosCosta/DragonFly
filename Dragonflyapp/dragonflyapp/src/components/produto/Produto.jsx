1f7872ddfe371b18ebb44f9afe6d144d576e62fd
RafaRamosCosta <rm94281@fiap.com.br>
2022-11-09 19:19:31 -0300

    Modificando layout do componente produto. Co-authored-by: Leonardo Ramos Costa <LeonardoRCosta@users.noreply.github.com> Co-authored-by: Mateus da Costa Leme <MateusLem@users.noreply.github.com> Co-authored-by: Heitor Borba Marini <heitorborbamarini@users.noreply.github.com> Co-authored-by: Diogo Kahn<DiogoKahn@users.noreply.github.com>

--- Dragonflyapp/dragonflyapp/src/components/produto/Produto.jsx
+++ Dragonflyapp/dragonflyapp/src/components/produto/Produto.jsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import tutorial from '../../videos/tutorial.mp4';
 import poster from '../../images/postertutorial.png';
+import cursoDragonfly from '../../images/cursoDragonfly.gif';
 import { Link } from 'react-router-dom';
-import { DivProd, DivVideoWrapper, PageProd } from './ProdutoStyle';
+import { DivCurso, DivProd, DivVideoWrapper, PageProd } from './ProdutoStyle';
 
 export default function Produto() {
   const userData = JSON.parse(sessionStorage.getItem('user'));
+
   const [produtos, setProdutos] = useState([]);
 
   useEffect(() => {
@@ -14,42 +16,50 @@ export default function Produto() {
       axios('http://localhost:8080/DragonflyAPI/rest/produto').then(
         (response) => setProdutos(response.data)
       );
-    } else {
-      window.location = '/login';
     }
+    // else {
+    //   window.location = '/login';
+    // }
   }, []);
+
   return (
     <PageProd>
       <h1 id="main-h1">
         {userData?.nmFantasia
-          ? `Aproveite nossas ofertas ${userData.nmFantasia}!`
+          ? `Olá ${userData.nmFantasia}, aproveite nossas ofertas!`
           : `Produtos cadastrados!`}
       </h1>
-       <a id="linkTutorial" href="#tutorial">Assista o Tutorial de Pilotagem</a>   
-      {produtos.map((produto, i) => (
-        <DivProd key={i}>
+      <h2>Confira o pacote do curso de pilotagem!</h2>
+
+      <Link to="/compraCursoPilotagem">
+        <DivCurso>
+          <div>
+            <img src={cursoDragonfly} alt="" />
+          </div>
+        </DivCurso>
+      </Link>
+      {produtos.map((produto) => (
+        <DivProd key={produto.idProdruto} id="divProd">
           <h1>{produto.nmProduto}</h1>
           <p>
             "<img src="imagem_drone" alt="" />"
           </p>
-          <h2>Descrição do Produto</h2>
-          <p style={{ color: '#fffa6b' }}>{produto.dsProduto}</p>
-          <h2>Preço do Produto</h2>
-          <p style={{ color: '#fff' }}>R$ {produto.vlUnitario}</p>
-          <Link to="algum_lugar_pra_relaxar" className="compra">
-            🛒Comprar
+          <p style={{ color: '#fff', fontWeight: 'bold', fontSize: '20px' }}>
+            R$ {produto.vlUnitario}
+          </p>
+          <Link
+            to="/compra"
+            className="addCarrinho"
+            onClick={() => sessionStorage.setItem('prodId', produto.idProduto)}>
+            + Ver mais
           </Link>
         </DivProd>
       ))}
+
       <section>
-        <DivVideoWrapper id='tutorial'>
-        <h1>Tutorial de Pilotagem</h1>
-          <video
-            controls
-            loop
-            src={tutorial}
-            poster={poster}
-          />
+        <DivVideoWrapper id="tutorial">
+          <h1>Tutorial de Configuração</h1>
+          <video controls loop src={tutorial} poster={poster}/>
         </DivVideoWrapper>
       </section>
     </PageProd>
--- Dragonflyapp/dragonflyapp/src/components/produto/ProdutoStyle.js
+++ Dragonflyapp/dragonflyapp/src/components/produto/ProdutoStyle.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const PageProd = styled.div`
   display: flex;
   flex-direction: column;
+  flex-wrap: wrap;
   align-items: center;
   justify-content: center;
   row-gap: 20px;
@@ -10,7 +11,10 @@ export const PageProd = styled.div`
   @media screen and (max-width: 768px) {
     #main-h1 {
       margin-top: 45px;
-      font-size: 1.6rem;
+      font-size: 1rem;
+    }
+    h2 {
+      font-size: 0.8rem;
     }
   }
   #linkTutorial {
@@ -18,7 +22,7 @@ export const PageProd = styled.div`
     text-decoration: none;
     border: #0c872b solid 2px;
     border-radius: 50px;
-    padding: 5px; 
+    padding: 5px;
     transition: all 0.3s ease-in-out;
     :hover {
       background-color: #0c872b;
@@ -31,23 +35,23 @@ export const DivProd = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  padding: 10px;
-  background-color: #0f521d;
+  padding: 8px;
+  background-color: #0c872b;
   border-radius: 7px;
-  max-width: 500px;
+  max-width: 400px;
   row-gap: 2px;
-  .compra {
+  .addCarrinho {
     text-align: center;
     border: none;
-
     background-color: #0c5c11;
     cursor: pointer;
-
     padding: 10px;
     border-radius: 5px;
     color: #fff;
+    font-size: 0.8rem;
     width: 40%;
     transition: all 0.3s ease-in-out;
+    text-decoration: none;
     :hover {
       background-color: #1c8d19;
       transform: scale(1.1);
@@ -72,10 +76,50 @@ export const DivVideoWrapper = styled.div`
   row-gap: 20px;
   justify-content: center;
   align-items: center;
+  max-width: 500px;
+  padding: 20px;
+  
   video {
+    border-radius: 10px;
     min-width: 300px;
-    max-width: 50%;
+    max-width: 100%;
     min-height: 200px;
-    max-height: 50%;
+    max-height: 100%;
+    transition: transform 0.3s ease-in-out;
+      :hover {
+        outline: 3px solid #35a14d;
+        transform: scale(1.07);
+      }
+  }
+  @media screen and (max-width: 768px) {
+    padding: 0;
+  }
+`;
+
+export const DivCurso = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 65vh;
+  div {
+    display: flex;
+    justify-content: center;
+    img {
+      align-self: center;
+      width: 60%;
+      min-width: 300px;
+      border-radius: 10px;
+      transition: transform 0.3s ease-in-out;
+      :hover {
+        outline: 3px solid #35a14d;
+        transform: scale(1.1);
+      }
+    }
+  }
+  @media screen and (max-width: 768px) {
+    height: 60vh;
+  }
+  @media screen and (max-width: 720px) {
+    height: 40vh;
   }
 `;

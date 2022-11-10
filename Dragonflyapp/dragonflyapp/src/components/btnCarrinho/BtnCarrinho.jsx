5dc6a2b32f50b8c547cf5bb33eaac4c89aa08356
RafaRamosCosta <rm94281@fiap.com.br>
2022-11-10 19:00:02 -0300

    Passando a função renderCarrinho por props para fechar ou abrir o carrinho ao clicar no botão. Co-authored-by: Leonardo Ramos Costa <LeonardoRCosta@users.noreply.github.com>

--- Dragonflyapp/dragonflyapp/src/components/btnCarrinho/BtnCarrinho.jsx
+++ Dragonflyapp/dragonflyapp/src/components/btnCarrinho/BtnCarrinho.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ShoppingCart from '../../images/shopping-cart.svg'
 import { Btn } from './BtnCarrinhoStyle'
-export default function BtnCarrinho() {
+export default function BtnCarrinho({ renderCarrinho }) {
   return (
-    <Btn>
+    <Btn onClick={renderCarrinho}>
         <img src={ShoppingCart} alt="Carrinho de compras" />
-        <div>3</div>
+        <div>{JSON.parse(localStorage.getItem('carrinho'))?.length || 0}</div>
     </Btn>
   )
 }
--- Dragonflyapp/dragonflyapp/src/components/btnCarrinho/BtnCarrinhoStyle.js
+++ Dragonflyapp/dragonflyapp/src/components/btnCarrinho/BtnCarrinhoStyle.js
@@ -15,15 +15,15 @@ export const Btn = styled.button`
   }
   div {
     display: flex;
-    justify-content: center;
-    align-items: center;
+    justify-content: center !important;
+    align-items: center !important;
     position: absolute;
     bottom: 0;
     right: 0;
     background-color: red;
     border-radius: 50%;
-    width: 15px;
-    height: 15px;
+    width: 15px !important;
+    height: 15px !important;
     font-size: 10px !important;
     text-align: center;
     transform: translate(45%, 25%);
